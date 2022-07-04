cc1ae220bbaf6a058a762834a7b8d6669393247e
MikeNichols1371 <mnich13711@gmail.com>
2022-07-04 12:47:31 -0400

    Completed MethodExercise 1

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("What is your name?");
+var name = Console.ReadLine();
+Console.WriteLine("What is your favorite color");
+var color = Console.ReadLine();
+Console.WriteLine("What is your favorite animal");
+var animal = Console.ReadLine();
+Console.WriteLine("What is your favorite music group");
+var musicGroup = Console.ReadLine();
